0624fc0111b124a61ad79033aa2292c573531d30
nshimiyumukiza <nshimiyumukizaerneste99@gmail.com>
2024-12-09 16:48:03 +0200

    adjust image paths

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className='bg-black text-white py-16 text-center px-12'>
       <div className='flex justify-center'>
-      <img src="src/assets/image.png" alt=""className='mx-autor mb-8 w-48 h-48 rounded-full object-cover transifrom transition-transiform 
+      <img src="./assets/image.png" alt=""className='mx-autor mb-8 w-48 h-48 rounded-full object-cover transifrom transition-transiform 
       duretion-300 hover:scale-105 ' />
       </div>
       <h1 className='text-4xl font-bold'>
