6aadb98f34ec8789f78f2b617ff0012d3eb39b81
nshimiyumukiza <nshimiyumukizaerneste99@gmail.com>
2024-12-18 15:28:16 +0200

    style

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,12 +4,12 @@ const Contact = () => {
     <div className='bg-sky-900 text-white '>
       <div className='flex flex-col justify-center pt-12'>
         <h1 className='text-3xl text-center font-bold p-8'>Contact with me</h1>
-        <div className='p-8 w-[300px] h-[450]px border items-center flex justify-center'>
-            <form className='space-y-4 pb-4'>
-        <input className='border outline-none text-black font-bold p-4' type="text"placeholder='Enter Name' /><br />
-        <input className='border outline-none text-black font-bold p-4' type="text" placeholder='Enter Email'/><br />
-        <textarea className='border outline-none text-black font-bold p-4' name="" id=""placeholder='type message'></textarea><br />
-        <button className='p-2 bg-blue-600'>Send</button>
+        <div className=' w-[300px] h-[450]px m-auto'>
+            <form className='space-y-4 border p-3'>
+                <input className=' rounded-md border outline-none text-black font-bold p-4' type="text"placeholder='Enter Name' /><br />
+                <input className=' rounded-md border outline-none text-black font-bold p-4' type="text" placeholder='Enter Email'/><br />
+                <textarea className=' rounded-md border outline-none text-black font-bold p-4' name="" id=""placeholder='type message'></textarea><br />
+                <button className=' rounded-md px-4 bg-blue-600'>Send</button>
             </form>
         </div>
       </div>
